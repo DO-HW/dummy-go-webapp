807d5808c14d2ea71bae3852144e86ac6ed46fe3
Pál Szák <szak.pal@gmail.com>
2015-07-13 14:28:58 +0200

    Use lib and add dependency

--- dgw.go
+++ dgw.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 )
 
+import "gitlab.com/DOHW/dummy-go-lib"
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello "+ sayhello.HelloWorld() +"!")
 }
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8000", nil)
 }
-
