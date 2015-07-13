b339f690a41334b3406a06c19514e3bc0ace17d5
Pál Szák <szak.pal@gmail.com>
2015-07-13 15:38:42 +0200

    Test for webapp

---
+++ dgw_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "testing"
+  "net/http"
+  "net/http/httptest"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestHello(t *testing.T){
+
+    req, _ := http.NewRequest("GET", "", nil)
+    w := httptest.NewRecorder()
+
+    hello(w, req)
+
+    if w.Code != http.StatusOK {
+        t.Errorf("Home page didn't return %v", http.StatusOK)
+    }
+
+    assert.Equal(t, w.Body.String(), "Hello World!", "body should be Hello World!");
+
+}
